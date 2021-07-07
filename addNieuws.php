04ea4be1c8fbbba65c8f75879acd458023bd5b05
Mark Kazemier <mark.kazemier@gmail.com>
2021-07-07 19:12:42 +0200

    Resolved errors in news comment form
    Resolves #16 and #17

--- addNieuws.php
+++ addNieuws.php
@@ -16,26 +16,28 @@ $cUser = new User();
 /* Verbinding met de database maken */
 connectDB();
 
-/* Permissies controleren */
-if (isset($_GET['id'])) {
-    if (isset($_POST['reactie'])) {
-        if (($cUser->checkSession()) || ($cUser->checkCookie())) {
-            $sQuery = "INSERT INTO nieuwsreacties (reactieid, nieuwsid, userid, bericht, datum, tijd)
-                 VALUES ('', '" . add($_GET['id']) . "', '" . $cUser->m_iUserid . "', 
-                 '" . add($_POST['reactie']) . "', NOW(), NOW());";
-            if (mysql_query($sQuery)) {
-                $cUser->addPost();
-                header('Location: shownieuws.php?id=' . $_GET['id']);
-            } else {
-                print 'Er is iets fout gegaan met de database en/of query';
-                print '<br>' . $sQuery . '<br>' . mysql_error();
-            }
-        } else {
-            header('Location: loginForm.php');
-        }
-    } else {
-        header('HTTP/1.0 404 Page not Found');
+try {
+    if(!isset($_GET['id'])) {
+        throw new Exception('Missing news id parameter');
     }
-} else {
+
+    if($_SERVER['REQUEST_METHOD'] != 'POST' || !isset($_POST['reactie'])) {
+        throw new Exception('Comment form isn\'t posted');
+    }
+
+    if (($cUser->checkSession()) || ($cUser->checkCookie())) {
+        $sQuery = "INSERT INTO nieuwsreacties (nieuwsid, userid, bericht, datum, tijd)
+         VALUES ('" . add($_GET['id']) . "', '" . $cUser->m_iUserid . "', 
+         '" . add($_POST['reactie']) . "', NOW(), NOW());";
+        if (!mysql_query($sQuery)) {
+            throw new Exception('Error adding comment to the news item');
+        }
+
+        $cUser->addPost();
+        header('Location: shownieuws.php?id=' . $_GET['id']);
+    } else {
+        header('Location: loginForm.php');
+    }
+} catch (Exception $e) {
     header('HTTP/1.0 404 Page not Found');
 }
