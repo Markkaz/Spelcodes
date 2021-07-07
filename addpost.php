405062e4f4eb15f4e0dd0d7a4afddb6e07d20260
Mark Kazemier <mark.kazemier@gmail.com>
2021-07-07 19:43:33 +0200

    Added test coverage for add game comment page
    Increases test coverage (#9)
    And revealed issues #18, #19 and #20

--- addpost.php
+++ addpost.php
@@ -1,47 +1,37 @@
-<?php
-error_reporting(E_ALL);
-
-session_start();
-
-/* Classes importeren */
-include('Classes/User.php');
-include('Classes/Template.php');
-
-/* Includes importeren */
-include('Includes/connect.php');
-include('Includes/slashes.php');
-
-/* Classes initialiseren */
-$cUser = new User();
-
-/* Verbinden met mysql */
-connectDB();
-
-/* Permissies controleren */
-if((isset($_GET['topicid'])) && (isset($_POST['reactie'])) && (isset($_GET['id'])))
-{
-  if(($cUser -> checkSession()) || ($cUser -> checkCookie()))
-  {
-    $sQuery = "INSERT INTO berichten (berichtid, topicid, userid, bericht, datum, tijd)
-               VALUES ('', '" . add($_GET['topicid']) . "', '" . $cUser -> m_iUserid . "',
-               '" . add($_POST['reactie']) . "', NOW(), NOW());";
-    if(mysql_query($sQuery))
-    {
-      $cUser -> addPost();
-      header('Location: gameview.php?id=' . $_GET['id'] . '&topicid=' . $_GET['topicid']);
-    }
-    else
-    {
-      Print 'Er is iets niet in orde met de database';
-    }
-  }
-  else
-  {
-    header('Location: loginForm.php');
-  }
-}
-else
-{
-  header('HTTP/1.0 404 Page not Found');
-}
-?>
+<?php
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+session_start();
+
+/* Classes importeren */
+include_once('Classes/User.php');
+include_once('Classes/Template.php');
+
+/* Includes importeren */
+include_once('Includes/connect.php');
+include_once('Includes/slashes.php');
+
+/* Classes initialiseren */
+$cUser = new User();
+
+/* Verbinden met mysql */
+connectDB();
+
+/* Permissies controleren */
+if ((isset($_GET['topicid'])) && (isset($_POST['reactie'])) && (isset($_GET['id']))) {
+    if (($cUser->checkSession()) || ($cUser->checkCookie())) {
+        $sQuery = "INSERT INTO berichten (berichtid, topicid, userid, bericht, datum, tijd)
+               VALUES ('', '" . add($_GET['topicid']) . "', '" . $cUser->m_iUserid . "',
+               '" . add($_POST['reactie']) . "', NOW(), NOW());";
+        if (mysql_query($sQuery)) {
+            $cUser->addPost();
+            header('Location: gameview.php?id=' . $_GET['id'] . '&topicid=' . $_GET['topicid']);
+        } else {
+            print 'Er is iets niet in orde met de database';
+        }
+    } else {
+        header('Location: loginForm.php');
+    }
+} else {
+    header('HTTP/1.0 404 Page not Found');
+}
