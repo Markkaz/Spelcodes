a3f0ac1c2ca864dbfc97f6669b25e2529ff6f9ac
Mark Kazemier <mark.kazemier@gmail.com>
2021-07-07 22:19:29 +0200

    Added test coverage for incoming links counter
    Relates to #9

--- tests/Pages/LinksTestTest.php
+++ tests/Pages/LinksTestTest.php
@@ -20,4 +20,42 @@ class LinksTestTest extends TestCase
         $this->assertContains('Spelcodes', $page);
         $this->assertContains('Webdevils', $page);
     }
+
+    /** @test */
+    public function it_counts_outgoing_clicks()
+    {
+        $linkId = LinkFactory::create(self::$pdo, 'Spelcodes', 'https://spelcodes.nl');
+
+        $page = $this->visitPage(
+            __DIR__ . '/../../uit.php',
+            ['id' => $linkId]
+        );
+
+        $this->assertEquals('', $page);
+        $this->assertDatabaseHas('links', [
+            'link' => 'Spelcodes',
+            'outcomming' => 1
+        ]);
+    }
+
+    /** @test */
+    public function it_shows_404_when_id_isnt_provided()
+    {
+        $page = $this->visitPage(
+            __DIR__ . '/../../uit.php'
+        );
+
+        $this->assertEquals('', $page);
+    }
+
+    /** @test */
+    public function it_shows_404_when_link_doesnt_exist()
+    {
+        $page = $this->visitPage(
+            __DIR__ . '/../../uit.php',
+            ['id' => 999]
+        );
+
+        $this->assertEquals('', $page);
+    }
 }
