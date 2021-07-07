151df8908658bbbc5c71eb3fae41f374ac86d989
Mark Kazemier <mark.kazemier@gmail.com>
2021-07-07 18:15:34 +0200

    Invalid news id will return 404 error
    Resolved #15

--- shownieuws.php
+++ shownieuws.php
@@ -21,50 +21,64 @@ $cTPL = new Template('Templates/main.tpl');
 connectDB();
 
 /* Controleren of er wel een id is meegegeven */
-if (isset($_GET['id'])) {
+try {
+    if(!isset($_GET['id'])) {
+        throw new Exception('Missing id parameter');
+    }
+
     include('Includes/login.php');
 
     $cTPL->setFile('CONTENT', 'Templates/shownieuws.tpl');
     $cTPL->parse();
 
     $sQuery = "SELECT n.titel, n.bericht, u.username, n.datum, n.tijd FROM nieuws n, users u
-             WHERE u.userid = n.userid AND n.nieuwsid='" . add($_GET['id']) . "';";
-    if ($cResult = mysql_query($sQuery)) {
-        $aData = mysql_fetch_assoc($cResult);
-        $cTPL->setPlace('TITEL', add($aData['titel']));
-        $cTPL->setPlace('USERNAME', add($aData['username']));
-        $cTPL->setPlace('BERICHT', smiley(strip($aData['bericht'])));
-        $cTPL->setPlace('DATUM', add($aData['datum'] . ' ' . $aData['tijd']));
-        $cTPL->parse();
+         WHERE u.userid = n.userid AND n.nieuwsid='" . add($_GET['id']) . "';";
+    $cResult = mysql_query($sQuery);
+    if(!$cResult) {
+        throw new Exception('Error loading news item');
     }
+
+    $aData = mysql_fetch_assoc($cResult);
+    if(!$aData) {
+        throw new Exception('News item doesn\'t exist');
+    }
+
+    $cTPL->setPlace('TITEL', add($aData['titel']));
+    $cTPL->setPlace('USERNAME', add($aData['username']));
+    $cTPL->setPlace('BERICHT', smiley(strip($aData['bericht'])));
+    $cTPL->setPlace('DATUM', add($aData['datum'] . ' ' . $aData['tijd']));
+    $cTPL->parse();
 
     $cTPL->setPlace('ID', $_GET['id']);
 
     $sQuery = "SELECT n.reactieid, n.bericht, u.userid, u.username, n.datum, n.tijd FROM nieuwsreacties n, users u
-             WHERE n.userid=u.userid AND n.nieuwsid='" . add($_GET['id']) . "'
-             ORDER BY n.datum, n.tijd;";
-    if ($cResult = mysql_query($sQuery)) {
-        while ($aData = mysql_fetch_assoc($cResult)) {
-            $cTPL->setBlock('REACTIES', 'reacties');
+         WHERE n.userid=u.userid AND n.nieuwsid='" . add($_GET['id']) . "'
+         ORDER BY n.datum, n.tijd;";
+    $cResult = mysql_query($sQuery);
+    if(!$cResult) {
+        throw new Exception('Error loading news comments');
+    }
+
+    while ($aData = mysql_fetch_assoc($cResult)) {
+        $cTPL->setBlock('REACTIES', 'reacties');
+        $cTPL->parse();
+
+        $cTPL->setPlace('AUTEUR', add($aData['username']));
+        $cTPL->setPlace('MOMENT', add($aData['datum'] . ' ' . $aData['tijd']));
+        $cTPL->setPlace('REACTIE', bbcode(smiley(strip_tags(strip($aData['bericht'])))));
+
+        if (($cUser->m_iPermis & 2) || ($aData['userid'] == $cUser->m_iUserid)) {
+            $cTPL->setBlock('REACTIEEDIT', 'edit');
             $cTPL->parse();
-
-            $cTPL->setPlace('AUTEUR', add($aData['username']));
-            $cTPL->setPlace('MOMENT', add($aData['datum'] . ' ' . $aData['tijd']));
-            $cTPL->setPlace('REACTIE', bbcode(smiley(strip_tags(strip($aData['bericht'])))));
-
-            if (($cUser->m_iPermis & 2) || ($aData['userid'] == $cUser->m_iUserid)) {
-                $cTPL->setBlock('REACTIEEDIT', 'edit');
-                $cTPL->parse();
-                $cTPL->setPlace('REACTIEID', $aData['reactieid']);
-            } else {
-                $cTPL->setPlace('REACTIEEDIT', '');
-            }
-
-            $cTPL->parse();
+            $cTPL->setPlace('REACTIEID', $aData['reactieid']);
+        } else {
+            $cTPL->setPlace('REACTIEEDIT', '');
         }
+
+        $cTPL->parse();
     }
 
     $cTPL->show();
-} else {
+} catch(Exception $e) {
     header('HTTP/1.0 404 Page not Found');
 }
