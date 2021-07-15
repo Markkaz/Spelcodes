bb62f50dd58d63ccdb2fbfdbfa78187b89cc402f
Mark Kazemier <mark.kazemier@gmail.com>
2021-07-15 22:36:13 +0200

    Fixed identified bugs and security issues in edit forum topic
    Fixed #62
    Fixed #63
    Fixed #64
    Fixed #65

--- forum/topicBewerk.php
+++ forum/topicBewerk.php
@@ -15,14 +15,20 @@ try {
     /* Controleren of je bent ingelogd */
     if ((!$cUser->checkSession()) && (!$cUser->checkCookie())) {
         header('Location: ../loginForm.php');
+        throw new ExitException();
     }
 
     /* Data ophalen */
     $sQuery = "SELECT forum_id, topic_titel, topic_poster FROM forum_topics WHERE topic_id='" . add($_GET['topicid']) . "';";
     if ($cResult = mysql_query($sQuery)) {
         $aData = mysql_fetch_assoc($cResult);
-        if ((!$cUser->m_iUserid == $aData['topic_poster']) && (!$cUser->m_iPermis & 2)) {
-            die('Geen permissie...');
+        if($aData === false) {
+            header('Http/1.0 404 Not Found');
+            throw new ExitException();
+        }
+        if (($cUser->m_iUserid != $aData['topic_poster']) && !($cUser->m_iPermis & 2)) {
+            header('Http/1.0 404 Not Found');
+            throw new ExitException();
         }
 
         /* Controleren of het formulier is verzonden */
@@ -31,6 +37,7 @@ try {
                WHERE topic_id='" . add($_GET['topicid']) . "';";
             if (mysql_query($sQuery)) {
                 header('Location: viewForum.php?forumid=' . $aData['forum_id']);
+                throw new ExitException();
             } else {
                 $cTPL->setPlace('TITEL', 'Fout met database');
                 $cTPL->setPlace('CONTENT', 'Door een fout met de database, is je request niet verwerkt.');
