fba388d1d6895aad5e967cf6084ef9b575b40cc1
Mark Kazemier <mark.kazemier@gmail.com>
2021-07-15 21:11:53 +0200

    Added test coverage on the forum reply functionality
    And identified three bugs: #50 , #51 and #52

--- tests/Factories/ForumTopicFactory.php
+++ tests/Factories/ForumTopicFactory.php
@@ -9,7 +9,7 @@ class ForumTopicFactory
         $sql = 'INSERT INTO forum_topics
                     (forum_id, topic_titel, topic_poster, topic_time, topic_replies, topic_status, topic_views, last_post, last_post_Time)
                 VALUES 
-                    (?, ?, ?, NOW(), 0, ?, 0, 999, NOW())';
+                    (?, ?, ?, NOW(), 0, ?, 0, 0, NOW())';
         $query = $pdo->prepare($sql);
         $query->execute([
             $forumId, $title, $userId, $status
