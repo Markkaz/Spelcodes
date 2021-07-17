facf0e78a83a4a2ea52d0297d5d69722349b7ce4
Mark Kazemier <mark.kazemier@gmail.com>
2021-07-17 12:48:37 +0200

    Resolved all known bugs related to adding forum topics
    Fixed #71
    Fixed #72
    Fixed #73
    Fixed #74
    Fixed #75

--- Includes/connect.php
+++ Includes/connect.php
@@ -1,1 +1,37 @@
-<?phpdefine('MYSQL_USER', 'root');define('MYSQL_PASS', '');define('MYSQL_HOST', 'localhost');define('MYSQL_DB', 'spelcodes');function connectDB(){  if(!$cDBH = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS))  {    return false;  }  if(!mysql_select_db(MYSQL_DB, $cDBH))  {    return false;  }  return $cDBH;}?>
+<?php
+
+define('MYSQL_USER', 'homestead');
+
+define('MYSQL_PASS', 'secret');
+
+define('MYSQL_HOST', 'localhost');
+
+define('MYSQL_DB', 'spelcodes');
+
+
+
+function connectDB()
+
+{
+
+  if(!$cDBH = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS))
+
+  {
+
+    return false;
+
+  }
+
+  if(!mysql_select_db(MYSQL_DB, $cDBH))
+
+  {
+
+    return false;
+
+  }
+
+  return $cDBH;
+
+}
+
+?>
